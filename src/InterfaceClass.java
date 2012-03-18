296232b55ab54de5e68a810a0e98c5390ab99afd
Joe Carvalho <sephiroth_hp@hotmail.com>
2012-03-18 04:13:00 -0300

    Teste e classe interface

---
+++ src/InterfaceClass.java
@@ -0,0 +1,7 @@
+
+public interface InterfaceClass {
+
+@overide
+public interface
+
+}
---
+++ src/TesteCase.java
@@ -0,0 +1,19 @@
+import junit.framework.TestCase;
+
+
+public class TesteCase extends TestCase {
+
+	//erro sendo testado
+    public void listar() throws Exception {  
+    	
+        try {  
+
+        
+        } catch (Exception e) {   
+        	
+        }  
+    }  
+
+    
+
+}
